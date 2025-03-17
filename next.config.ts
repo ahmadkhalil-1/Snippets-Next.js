4e57cdc09fa7c7f92913d0313963ac72f73f8b7d
ahmadkhalil-1 <ahmad5233388@gmail.com>
2025-03-18 03:33:56 +0500

    okk

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.experiments = { layers: true }; // Ensure Webpack is used
-    return config;
-  },
+  
 };
 
 export default nextConfig;
